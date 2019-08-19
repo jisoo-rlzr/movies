9562873998cb37111db18cd4d2ca16a7328defe1
jisoo <jsoo744@gmail.com>
2019-08-20 01:39:26 +0900

    Updates

---
+++ precache-manifest.1826df3915bef6b2eefca9dc41061254.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/movies/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "04129ba559ae92aa086e",
+    "url": "/movies/static/js/runtime~main.712e9881.js"
+  },
+  {
+    "revision": "7dc30a4470052ca4d8a1",
+    "url": "/movies/static/js/main.c8ef0107.chunk.js"
+  },
+  {
+    "revision": "7b84b94a0152dffaa438",
+    "url": "/movies/static/js/2.1d057864.chunk.js"
+  },
+  {
+    "revision": "7dc30a4470052ca4d8a1",
+    "url": "/movies/static/css/main.a78a2d46.chunk.css"
+  },
+  {
+    "revision": "0a6b51bb4388f50b2ab65947329d5cee",
+    "url": "/movies/index.html"
+  }
+];
